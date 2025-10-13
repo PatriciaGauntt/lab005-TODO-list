971b893fad4261ee0beb5a170b169e666764cb18
curiouskarma <plgauntt@hotmail,com>
2025-10-13 18:45:53 -0500

    corrected index

--- models/todolist.model.js
+++ models/todolist.model.js
@@ -3,57 +3,56 @@ import { logger } from '../lib/logger.js';
 import { mongo } from '../lib/mongo.js';
 
 export class ToDoListModel {
+  static getCollection() {
+    const db = mongo.getDb();
+    const collection = db.collection(Constants.TODOLIST_COLLECTIONS);
+
+    collection.createIndex({ id: 1 }, { unique: true }).catch(console.error);
+
+    return collection;
+  }
+
   static getToDoLists() {
     logger.debug('ToDoListModel : getToDoLists()');
-    return mongo.getDb().collection(Constants.TODOLIST_COLLECTIONS).find({}, {
-      projection: {
-        _id: 0,
-      },
+    return this.getCollection().find({}, {
+      projection: { _id: 0 },
     });
   }
 
   static getToDoList(id) {
     logger.debug(`Model : getToDoList, id: ${id}`);
-    return mongo.getDb().collection(Constants.TODOLIST_COLLECTIONS).findOne({ id }, {
-      projection: {
-        _id: 0,
-      },
+    return this.getCollection().findOne({ id }, {
+      projection: { _id: 0 },
     });
   }
 
   static async createToDoList(toDoList) {
     logger.debug('Model : createToDoList');
-    await mongo.getDb().collection(Constants.TODOLIST_COLLECTIONS).insertOne(toDoList);
+    const collection = this.getCollection();
+    await collection.insertOne(toDoList);
     delete toDoList._id;
     return toDoList;
   }
 
   static async updateToDoList(id, toDoList) {
     logger.debug(`Model : updateToDoList, id: ${id}`);
-    const updateStatement = {
-      $set: {},
-    };
+    const updateStatement = { $set: {} };
 
     Object.keys(toDoList).forEach((key) => {
-      if (key === 'id') {
-        return;
-      }
+      if (key === 'id') return;
       updateStatement.$set[key] = toDoList[key];
     });
-    const result = await mongo.getDb().collection(Constants.TODOLIST_COLLECTIONS).updateOne(
-      { id },
-      updateStatement,
-      { returnDocument: 'after' },
-    );
 
+    const collection = this.getCollection();
+    const result = await collection.updateOne({ id }, updateStatement, { returnDocument: 'after' });
     delete result._id;
     return result;
   }
 
   static async replaceToDoList(id, toDoList) {
     logger.debug(`Model : replaceToDoList, id: ${id}`);
-    const result = await mongo.getDb().collection(Constants.TODOLIST_COLLECTIONS)
-      .replaceOne({ id }, toDoList);
+    const collection = this.getCollection();
+    const result = await collection.replaceOne({ id }, toDoList);
 
     if (!result.modifiedCount) {
       return false;
@@ -64,12 +63,9 @@ export class ToDoListModel {
 
   static async deleteToDoList(id) {
     logger.debug(`Model : deleteToDoList, id: ${id}`);
-    const result = await mongo.getDb().collection(Constants.TODOLIST_COLLECTIONS).deleteOne(
-      { id },
-    );
-    if (result.deletedCount) {
-      return true;
-    }
-    return false;
+    const collection = this.getCollection();
+    const result = await collection.deleteOne({ id });
+
+    return result.deletedCount > 0;
   }
 }
